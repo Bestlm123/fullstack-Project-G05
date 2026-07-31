6dd3cbe3e0cd82d4462f9889d7d56e43fad6923e
Thittawin2548 <ptkk00177@gmail.com>
2026-07-31 16:29:25 +0700

    delete comment^^

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,3 @@
-// // src/index.ts
-// import express from 'express';
-// import adminRouter from './routes/admin.js';
-
-// const app = express();
-// app.use(express.json());
-
-// // Routes
-// app.use('/api', adminRouter);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-// // ✅ Export app ออกไปเพื่อใช้ทำ Supertest
-// export default app;
-
 import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
